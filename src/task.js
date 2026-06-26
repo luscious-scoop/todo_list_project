ec3e1fd020c3da35b99ca958976d2dbf6c333c9b
Haseeb <ah085904@gmail.com>
2026-06-26 18:37:29 +0500

    add class to create a todo

---
+++ src/task.js
@@ -0,0 +1,14 @@
+class toDoTask {
+	constructor(title, description, dueDate, priority) {
+		this.id = crypto.randomUUID();
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.priority = priority;
+		this.isCompleted = false;
+	}
+
+	toggleStatus() {
+		this.isCompleted = !this.isCompleted;
+	}
+}
