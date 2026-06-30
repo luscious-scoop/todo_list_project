105ab58082569764eed1c0482c990f791ebde020
Haseeb <ah085904@gmail.com>
2026-06-30 18:29:41 +0500

    add reference to dialog in screen controller

--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,6 @@
 function screenController() {
 	const homeBtn = document.querySelector(".home");
 	const notesBtn = document.querySelector("notes");
-	const completedBtn = document.querySelector("");
+	const completedBtn = document.querySelector("completed");
+	const dialog = document.querySelector("dialog");
 }
