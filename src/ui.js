39de72bf123501c43fb04778f1ba62e839581a8e
Haseeb <ah085904@gmail.com>
2026-06-30 15:07:29 +0500

    fix: completed button class name

--- src/ui.js
+++ src/ui.js
@@ -1,1 +1,5 @@
-function screenController() {}
+function screenController() {
+	const homeBtn = document.querySelector(".home");
+	const notesBtn = document.querySelector("notes");
+	const completedBtn = document.querySelector("");
+}
