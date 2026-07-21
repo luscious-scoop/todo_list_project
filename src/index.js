ec14fd12653126f1583cf242541d2d4dbbbdd388
Haseeb <ah085904@gmail.com>
2026-07-21 16:29:42 +0500

    chore: remove non used imports from index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import "./style.css";
 
-import {
-	taskController,
-	notes,
-	task,
-	notesController,
-	createToDoObject,
-} from "./task.js";
-import { projectController } from "./projects.js";
 import { screenController } from "./ui.js";
 
 screenController();
